b731bb9e6910bcb30f78d297d78d7db871c56c63
Torrel Moseley <trrl.mo@hotmail.com>
2024-07-10 12:15:45 -0500

    add tests for other data

--- test/rate_limiter_test.rb
+++ test/rate_limiter_test.rb
@@ -2,11 +2,27 @@ require 'minitest/autorun'
 require_relative '../rate_limiter.rb'
 
 class TestRateLimiter < Minitest::Test
-  def test_blocked
+  def test_blocked_in_example_data
     path = './data/requests_example.csv'
     rate_limiter = RateLimiter.new(1, 60)
     blocked = rate_limiter.blocked(path)
 
     assert_equal(3, blocked)
+  end
+
+  def test_blocked_in_sample_data
+    path = './data/requests_sample.csv'
+    rate_limiter = RateLimiter.new(1, 60)
+    blocked = rate_limiter.blocked(path)
+
+    assert_equal(5, blocked)
+  end
+
+  def test_blocked_in_data
+    path = './data/requests.csv'
+    rate_limiter = RateLimiter.new(50, 60)
+    blocked = rate_limiter.blocked(path)
+
+    assert_equal(1446, blocked)
   end
 end
