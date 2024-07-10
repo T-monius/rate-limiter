d992bd070fc899761427848d10f507b8b3cea941
Torrel Moseley <trrl.mo@hotmail.com>
2024-07-10 13:16:37 -0500

    refactor lightly

--- rate_limiter.rb
+++ rate_limiter.rb
@@ -1,27 +1,33 @@
 require 'csv'
 require 'time'
 
+
 class RateLimiter
-  def initialize(max, t)
-    self.iprepmax = max
-    self.time_frame = t
+  Rule = Struct.new('Rule', :iprepmax, :time_frame)
+  def initialize(rule)
+    self.rule = rule
   end
 
   def blocked(path)
     seen = { ip: [] }
     blocked = []
-    CSV.foreach(path, headers: true) do |row|
-      requests = seen[row['request_ip']] || []
-      requests << row and seen[row['request_ip']] = requests
-      next if requests.size <= iprepmax
-      anchor_request = requests[-(iprepmax + 1)]
-      anchor_time = Time.xmlschema(anchor_request['timestamp'])
-      current_request_time = Time.xmlschema(row['timestamp'])
-      blocked << row unless current_request_time >= (anchor_time + 60)
+    CSV.foreach(path, headers: true) do |current_request|
+      requests_for_ip = seen[current_request['request_ip']] || []
+      requests_for_ip << current_request and seen[current_request['request_ip']] = requests_for_ip
+      next if requests_for_ip.size <= rule.iprepmax
+      # Identify request that is beginning of time window
+      anchor_request = requests_for_ip[-(rule.iprepmax + 1)]
+      blocked << current_request unless outside_time_frame?(anchor_request, current_request)
     end
     blocked.size
   end
 
+  def outside_time_frame?(anchor_request, current_request)
+    anchor_time = Time.xmlschema(anchor_request['timestamp'])
+    current_request_time = Time.xmlschema(current_request['timestamp'])
+    current_request_time >= (anchor_time + rule.time_frame)
+  end
+
   protected
-  attr_accessor :iprepmax, :time_frame
+  attr_accessor :rule
 end
--- test/rate_limiter_test.rb
+++ test/rate_limiter_test.rb
@@ -4,7 +4,8 @@ require_relative '../rate_limiter.rb'
 class TestRateLimiter < Minitest::Test
   def test_blocked_in_example_data
     path = './data/requests_example.csv'
-    rate_limiter = RateLimiter.new(1, 60)
+    rule = RateLimiter::Rule.new(1, 60)
+    rate_limiter = RateLimiter.new(rule)
     blocked = rate_limiter.blocked(path)
 
     assert_equal(3, blocked)
@@ -12,7 +13,8 @@ class TestRateLimiter < Minitest::Test
 
   def test_blocked_in_sample_data
     path = './data/requests_sample.csv'
-    rate_limiter = RateLimiter.new(1, 60)
+    rule = RateLimiter::Rule.new(1, 60)
+    rate_limiter = RateLimiter.new(rule)
     blocked = rate_limiter.blocked(path)
 
     assert_equal(5, blocked)
@@ -20,9 +22,23 @@ class TestRateLimiter < Minitest::Test
 
   def test_blocked_in_data
     path = './data/requests.csv'
-    rate_limiter = RateLimiter.new(50, 60)
+    rule = RateLimiter::Rule.new(50, 60)
+    rate_limiter = RateLimiter.new(rule)
     blocked = rate_limiter.blocked(path)
 
     assert_equal(1446, blocked)
   end
+
+  def test_within_time_frame
+    timestamp =         '2024-01-01T00:00:00+00:00'
+    another_timestamp = '2024-01-01T00:00:01+00:00'
+    anchor_request = MockRequest.new(timestamp)
+    current_request = MockRequest.new(another_timestamp)
+    rule = RateLimiter::Rule.new(1, 60)
+    rate_limiter = RateLimiter.new(rule)
+    is_outside_time_frame = rate_limiter.outside_time_frame?(anchor_request, current_request)
+
+    refute(is_outside_time_frame)
+  end
+  MockRequest = Struct.new('MockRequest', :timestamp)
 end
